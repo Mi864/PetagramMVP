c8b3869829a2273fe72e51465a2a0c95a0b4a214
Mi864 <69161845+Mi864@users.noreply.github.com>
2020-12-04 23:42:24 -0500

    Delete ConstantesRestApi.java

--- app/src/main/java/com/example/petagram/ConstantesRestApi.java
+++
@@ -1,8 +0,0 @@
-package com.example.petagram.RestApi;
-
-public class ConstantesRestApi {
-
-    public final static String URL_Servidor = "https://nameless-reaches-79777.herokuapp.com/";
-    public final static String PATH_Token = "token-device";
-
-}
